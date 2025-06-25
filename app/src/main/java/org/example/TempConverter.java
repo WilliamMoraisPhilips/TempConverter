c7b307614fb6038dbacb30e24462c1cfa7e75d30
William Morais Weidgenant <WilliamMorais.Weidgenant@philips.com>
2025-06-25 14:42:22 -0300

    Testes unitários

--- app/src/main/java/org/example/TempConverter.java
+++ app/src/main/java/org/example/TempConverter.java
@@ -13,7 +13,7 @@ public class TempConverter {
     public void setTipoConversao(String tipoConversao) {
         if (tipoConversao.isBlank()){
             throw new IllegalArgumentException("O tipo de conversão não pode estar vazio");
-        }else if(tipoConversao.equals("FAHRENHEINT")|| tipoConversao.equals("CELSIUS") ) {
+        }else if(tipoConversao.equals("FAHRENHEIT")|| tipoConversao.equals("CELSIUS") ) {
             this.tipoConversao = tipoConversao;
         }else{
             throw new IllegalArgumentException("O tipo de conversão escolhido não é válido. Tente novamente");
@@ -25,19 +25,20 @@ public class TempConverter {
     }
 
     public void setConversao(Double conversao) {
-        if(conversao == null || conversao == 0){
+        if(conversao == null || conversao.equals(0.0)){
             throw new IllegalArgumentException();
-        }else if (tipoConversao.equals("FAHRENHEINT")){
-            if (((conversao-32)*0.55) < 459.67) {
+        }else if (tipoConversao.equals("FAHRENHEIT")){
+            if ((conversao * 1.8) + 32 < -459.67) {
                 throw new IllegalArgumentException("O valor informado está sendo convertido para abaixo do zero absoluto, tente novamente com outro valor");
             }else{
-                this.conversao = (conversao-32)*0.55;
+                this.conversao = (conversao * 1.8) + 32;
+
             }
         } else if (tipoConversao.equals("CELSIUS")) {
-            if (((conversao*1.8) - 32) < -273.15){
+            if ((conversao-32)/ 1.8 < -273.15){
                 throw new IllegalArgumentException("O valor informado está sendo convertido para abaixo do zero absoluto, tente novamente com outro valor");
             }else {
-                this.conversao = (conversao * 1.8) - 32;
+                this.conversao = (conversao-32)/ 1.8;
             }
         }else{
             throw new IllegalArgumentException("O tipo de conversão é inválido, tente novamente");
@@ -51,7 +52,7 @@ public class TempConverter {
         TempConverter converter =  new TempConverter();
 
         while(converter.getTipoConversao() == null) {
-            System.out.println("Escolhe o tipo de conversão a ser feito (CELSIUS ou FAHRENHEIT");
+            System.out.println("Escolhe o tipo de conversão a ser feito (CELSIUS ou FAHRENHEIT)");
             converter.setTipoConversao(leitor.next().toUpperCase());
         }
 
